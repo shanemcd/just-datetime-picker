929525dfa65bb02fb07cd500333a820d4a10bc57
Shane McDonald <me@shanemcd.com>
2013-04-04 15:28:13 -0300

    Patch version for Rails 4

--- lib/just-datetime-picker/engine.rb
+++ lib/just-datetime-picker/engine.rb
@@ -1,7 +1,7 @@
 module Just
   module DateTimePicker
     class Engine < Rails::Engine
-      if Rails.version >= "3.1"
+      if Rails.version.to_s >= "3.1"
         initializer "Just DateTime Picker precompile hook" do |app|
           app.config.assets.precompile += %w(just_datetime_picker/base.css just_datetime_picker/nested_form_workaround.js)
         end
